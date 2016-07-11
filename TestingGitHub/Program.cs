786989547532774dbfe0186ad6706f8dd9c6b650
KatoNamus <vanzyl.theo@gmail.com>
2016-07-11 15:22:50 +0200

    First Commit.

---
+++ TestingGitHub/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace TestingGitHub
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			WriteLine($"Hello GitHub, it is now {DateTime.Now.ToString("d MMMM yyyy, HH:mm")}");			
+		}
+	}
+}
